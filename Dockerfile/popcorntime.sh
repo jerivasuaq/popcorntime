702290a5d940a5ad4be0744abe575a01a3fde5ad
jerivas <jerivas@uaq.edu.mx>
2015-07-14 14:28:58 -0500

    added start script

--- Dockerfile/popcorntime.sh
+++ Dockerfile/popcorntime.sh
@@ -1,11 +1,13 @@
-docker run --rm -it \
+docker run -it \
     --net host \
     --cpuset 0 \
     --memory 512mb \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     --device /dev/snd \
+    --device /dev/dri \
     --name popcorn \
-	jerivas/popcorn
+	jerivas/popcorn \
+    /opt/popcorntime/Popcorn-Time
 
     
